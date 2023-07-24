aa05e13c9306cd0a667786e6451f76a80e546198
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-24 15:53:40 +0300

    creat to_dict(self) to return a dictionary instead of writing one each time

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 with app.app_context():
     db.create_all()
@@ -38,21 +41,7 @@ def home():
 @app.route('/random')
 def get_random_cafe():
     random_cafe = random.choice(Cafe.query.all())
-    return jsonify(
-        cafe={
-            'id': random_cafe.id,
-            'name': random_cafe.name,
-            'map_url': random_cafe.map_url,
-            'img_url': random_cafe.img_url,
-            'location': random_cafe.location,
-            'seats': random_cafe.seats,
-            'has_toilet': random_cafe.has_toilet,
-            'has_wifi': random_cafe.has_wifi,
-            'has_sockets': random_cafe.has_sockets,
-            'can_take_calls': random_cafe.can_take_calls,
-            'coffee_price': random_cafe.coffee_price,
-        }
-    )
+    return jsonify(Cafe.to_dict(random_cafe))
 ## HTTP GET - Read Record
 
 ## HTTP POST - Create Record
